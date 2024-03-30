ec8660a2f746f3336167fde305c66b83c238859c
Anthony Shew <anthonyshew@gmail.com>
2024-03-29 23:11:26 -0600

    Changing view counts, noice.

--- scripts/incrementer.ts
+++ scripts/incrementer.ts
@@ -1,23 +1,33 @@
 import { db } from "#db/index.ts";
 import { channels } from "#db/schema.ts";
 import { randomNumberInRange } from "#lib/random-number-in-range.js";
+import { eq } from "drizzle-orm";
 
 const main = () => {
   setInterval(async () => {
     const channelList = await db.select().from(channels);
 
     await Promise.all(
-      channelList.map((channel) => {
+      channelList.map(async (channel) => {
         const coinFlip = Math.random() > 0.5;
+        const rand = randomNumberInRange({ min: 3, max: 10 });
         const addOrSubtractRandomNumberInRange = (startNum: number) =>
-          coinFlip
-            ? startNum + randomNumberInRange({ min: 3, max: 10 })
-            : startNum - randomNumberInRange({ min: 3, max: 10 });
-        db.update(channels).set({
-          activeViewers: addOrSubtractRandomNumberInRange(
-            channel.activeViewers,
-          ),
-        });
+          coinFlip ? startNum + rand : startNum - rand;
+
+        // Bounded between 25 and 99
+        const newViewerCount = Math.min(
+          99,
+          Math.max(25, addOrSubtractRandomNumberInRange(channel.activeViewers)),
+        );
+
+        console.log(newViewerCount);
+
+        await db
+          .update(channels)
+          .set({
+            activeViewers: newViewerCount,
+          })
+          .where(eq(channels.id, channel.id));
       }),
     );
   }, 1000);
