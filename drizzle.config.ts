2718ee87f4bfc9fb5979d51eef31c288330bceb7
Anthony Shew <anthonyshew@gmail.com>
2024-04-04 10:36:18 -0600

    Make the database URL for local into a constant.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from "drizzle-kit";
+import { localDbUrl } from "#db/index.ts";
 
 export default {
   schema: "./db/schema.ts",
   out: "./db/migrations",
   driver: "pg",
   dbCredentials: {
-    connectionString: process.env.POSTGRES_URL as string,
+    connectionString: process.env.VERCEL
+      ? (process.env.POSTGRES_URL as string)
+      : localDbUrl,
   },
 } satisfies Config;
