2f5f5ba519c3503ff1b07a04e3f7283c6701a4a6
Anthony Shew <anthonyshew@gmail.com>
2024-04-10 22:19:47 -0600

    as cache

--- lib/get-view-count.ts
+++ lib/get-view-count.ts
@@ -1,11 +1,11 @@
 import { db } from "#db/index.ts";
 import { channels } from "#db/schema.ts";
 import { eq } from "drizzle-orm";
-import { unstable_cache } from "next/cache";
+import { unstable_cache as cache } from "next/cache";
 import { sleep } from "./utils";
 
 /** Get a channel's active viewer count, revalidated every 5 seconds. */
-export const getViewCount = unstable_cache(
+export const getViewCount = cache(
   async (slug: string) => {
     // Simulating a network or larger system
     await sleep(2000);
